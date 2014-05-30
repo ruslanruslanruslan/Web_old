261c6a82a73e31e5da3ced2426e562a5b78837df
3axapob <3axapob@192.168.1.44>
2014-05-30 19:36:36 +0400

    nn

--- item.php
+++ item.php
@@ -4,7 +4,7 @@
  *                           advertising platforms
  *
  *                        Copyright (C) 2013 OSCLASS
- *
+ * 
  *       This program is free software: you can redistribute it and/or
  *     modify it under the terms of the GNU Affero General Public License
  *     as published by the Free Software Foundation, either version 3 of
