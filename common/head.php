bb3c860be468af085f726579f4abbc8c662c11b1
tetrapak07 <tetrapak07@gmail.com>
2014-07-17 19:48:17 +0300

    item_error_fix

--- common/head.php
+++ common/head.php
@@ -126,7 +126,7 @@ osc_run_hook('header');
         /** code for scroll and select menu **/
         jQuery("nav.navigation ul.nav > li > a").each(function(b, c) {
             var div_Id = jQuery(this).attr("href");
-            jQuery(div_Id).data({"index": b});
+           // jQuery(div_Id).data({"index": b});
         });
         /** code for scroll and select menu **/
 
