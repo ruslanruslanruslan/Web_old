85842bc92de8090419a3c01309861cda84ff4383
tetrapak07 <tetrapak07@gmail.com>
2014-07-28 22:18:52 +0300

    Revert "Revert "Revert "different_changes3"""
    This reverts commit ba59c2950c8ced1f96e110a0be256c8c44bc1e84.

--- common/head.php
+++ common/head.php
@@ -126,15 +126,9 @@ osc_run_hook('header');
         /** code for scroll and select menu **/
         jQuery("nav.navigation ul.nav > li > a").each(function(b, c) {
             var div_Id = jQuery(this).attr("href");
-           // console.log(div_Id);
-            var pos =div_Id.indexOf('/item/new');
-           // console.log(pos);
-
-            if (pos===-1)
-            {
+        
+		    if ((div_Id!='http://playandbay.com/item/new#')&&(div_Id!='http://www.playandbay.com/item/new#')&&(div_Id!='http://www.playandbay.com/item/new')&&(div_Id!='http://playandbay.com/item/new'))
             jQuery(div_Id).data({"index": b});
-            // console.log(pos);
-            }
         });
         /** code for scroll and select menu **/
 
